58c48bbb7ecc58b9c552c1039585afa6cb61a22b
Admin <Admin@DESKTOP-62E5ITC>
2024-11-27 08:46:15 +0800

    iloveu

---
+++ src/main/java/com/mycompany/macmac/MACMAC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.macmac;
+
+/**
+ *
+ * @author Admin
+ */
+public class MACMAC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
